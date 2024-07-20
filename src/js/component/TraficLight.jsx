a957d72f2e3cb016d42f62d62427dd4b3e6713f2
Bernardo Patiño <bap_link@hotmail.com>
2024-07-20 09:49:03 +0000

    primer commit

---
+++ src/js/component/TraficLight.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TraficLight = () => {
+
+this.props.
+  return (
+    <div>TraficLight <h1>Imagen de un Semáforo</h1>
+       <img src="https://pngimg.com/uploads/traffic_light/traffic_light_PNG56238.png" alt="Semáforo"/>
+        </div>
+  )
+}
+
+export default TraficLight
