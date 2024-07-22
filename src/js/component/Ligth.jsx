5f34c3b283cc70e776546ffc63c4626937232cd1
Bernardo Patiño <bap_link@hotmail.com>
2024-07-22 20:05:34 +0000

    luces onclik redy

--- src/js/component/Ligth.jsx
+++ src/js/component/Ligth.jsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
 
 const Ligth = () => {
-  let ligthOff = "" ;
-  const ligthOn = [ligthOn, setLigthOn] = useState(on);
+  const colors = ["red", "amber", "green", "purple"];
 
-  
+  const [color, setColor] = useState("red");
 
   return (
-    <div className="trafic-ligth d-flex align-items-center flex-column">
-      <div id="luz-roja" className=" luz"></div>
-      <div id="luz-amarilla" className=" luz"></div>
-      <div id="luz-verde" className="luz"></div>
+    <div className="container">
+      <div className="trafic-ligth d-flex align-items-center flex-column">
+        <div
+          id="red"
+          className={`luz ${color === "redOn" ? "redOn" : "red"}`}
+          onClick={() => {
+            setColor("redOn");
+          }}
+        ></div>
+        <div
+          id="ambar"
+          className={`luz ${color === "ambarOn" ? "ambarOn" : "ambar"}`}
+          onClick={() => {
+            setColor("ambarOn");
+          }}
+        ></div>
+        <div
+          id="green"
+          className={`luz ${color === "greenOn" ? "greenOn" : "green"}`}
+          onClick={() => {
+            setColor("greenOn");
+          }}
+        ></div>
+      </div>
+      <div className="">
+        <button type="button" class="btn btn-outline-danger">
+          Danger
+        </button>
+        <button type="button" class="btn btn-outline-warning">
+          Warning
+        </button>
+        <button type="button" class="btn btn-outline-success">
+          Success
+        </button>
+      </div>
     </div>
   );
 };
